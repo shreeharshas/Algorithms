37d9f37d4e43214f53cbfa1ca778af176645a251
Shree Harsha S <shreeharshas@users.noreply.github.com>
2016-03-31 23:24:13 -0400

    Update time_conversion.py
    added comments

--- time_conversion.py
+++ time_conversion.py
@@ -1,3 +1,28 @@
+# File           : diagonalDifference.cs
+# Author         : Shree Harsha Sridharamurthy
+# Author email   : s.shreeharsha@gmail.com
+# Disclaimer     : For use by concerned personnel only. Released under MIT License - permitted to use this but need to quote the source origin.
+# Program        : Solution for hackerrank question posted here: https://www.hackerrank.com/challenges/time-conversion
+''' Description    : 
+Given a time in AM/PM format, convert it to military (2424-hour) time.
+
+Note: Midnight is 12:00:00AM12:00:00AM on a 1212-hour clock and 00:00:0000:00:00 on a 2424-hour clock. Noon is 12:00:00PM12:00:00PM on a 1212-hour clock and 12:00:00PM12:00:00PM on a 2424-hour clock.
+
+Input Format
+
+A single string containing a time in 1212-hour clock format (i.e.: hh:mm:ssAMhh:mm:ssAM or hh:mm:ssPMhh:mm:ssPM), where 01≤hh≤1201≤hh≤12.
+
+Output Format
+
+Convert and print the given time in 2424-hour format, where 00≤hh≤2300≤hh≤23.
+
+Sample Input
+
+07:05:45PM
+Sample Output
+
+19:05:45
+'''
 #!/bin/python
 
 import sys
