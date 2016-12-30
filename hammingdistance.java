42aebb7c6f1dea187651562c74340ee9b3331716
Shree Harsha Sridharamurthy <shreeharshas@users.noreply.github.com>
2016-12-29 23:51:22 -0500

    Update hammingdistance.java

--- hammingdistance.java
+++ hammingdistance.java
@@ -1,14 +1,5 @@
 public class hammingdistance {
     public int hammingDistance(int x, int y) {
-        String s1 = Integer.toBinaryString(x);
-        String s2 = Integer.toBinaryString(y);
-        int z = x ^ y;
-        int count = 0;
-        String s = Integer.toBinaryString(z);
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1')
-                count++;
-        }
-        return count;
+        return Integer.bitCount(x ^ y);
     }
 }
