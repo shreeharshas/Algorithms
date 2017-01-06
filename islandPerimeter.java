5bf27df922a228f4c07de5a200c41417210cfae1
Shree Harsha Sridharamurthy <shreeharshas@users.noreply.github.com>
2017-01-05 20:29:40 -0500

    reduced code size

--- islandPerimeter.java
+++ islandPerimeter.java
@@ -2,24 +2,6 @@
 Solution to find the perimeter of an island. Problem given at https://leetcode.com/submissions/detail/88070798/
 */
 public class islandPerimeter {
-    //method to check if the given cell is is the border or not
-    boolean checkIsland(String neighbour, int i, int j, int maxRow, int maxColumn){
-        switch(neighbour){
-            case "left": 
-                if(j==0) return false;
-                else return true;
-            case "right": 
-                if(j==maxColumn-1) return false;
-                else return true;
-            case "top": 
-                if(i==0) return false;
-                else return true;
-            case "bottom": 
-                if(i==maxRow-1) return false;
-                else return true;
-        }
-        return false;
-    }
     public int islandPerimeter(int[][] grid) {
         int perimeter = 0;
         int maxRow = grid.length;
@@ -30,10 +12,10 @@ public class islandPerimeter {
                     //check left right top and bottom
                     //if they are 1, increment counter
                     //handled edge case
-                    perimeter = (checkIsland("left", i, j, maxRow, maxColumn))?(grid[i][j-1]==0?perimeter+1:perimeter):perimeter+1;
-                    perimeter = (checkIsland("right", i, j, maxRow, maxColumn))?(grid[i][j+1]==0?perimeter+1:perimeter):perimeter+1;
-                    perimeter = (checkIsland("top", i, j, maxRow, maxColumn))?(grid[i-1][j]==0?perimeter+1:perimeter):perimeter+1;
-                    perimeter = (checkIsland("bottom", i, j, maxRow, maxColumn))?(grid[i+1][j]==0?perimeter+1:perimeter):perimeter+1;
+                    perimeter = (j!=0)?(grid[i][j-1]==0?perimeter+1:perimeter):perimeter+1;
+                    perimeter = (j!=maxColumn-1)?(grid[i][j+1]==0?perimeter+1:perimeter):perimeter+1;
+                    perimeter = (i!=0)?(grid[i-1][j]==0?perimeter+1:perimeter):perimeter+1;
+                    perimeter = (i!=maxRow -1)?(grid[i+1][j]==0?perimeter+1:perimeter):perimeter+1;
                 }
             }
         }
