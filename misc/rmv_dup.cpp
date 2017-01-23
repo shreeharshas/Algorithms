dfb9822f55b230540fdf613b1158f428fa6d618e
Shree Harsha Sridharamurthy <shreeharshas@users.noreply.github.com>
2017-01-23 00:01:02 -0500

    Update rmv_dup.cpp

--- misc/rmv_dup.cpp
+++ misc/rmv_dup.cpp
@@ -1,28 +1,28 @@
+//C++ program to remove duplicates in a linked list
 include <iostream>
-include <unordered_map>
-using namespace std;
-
-struct Node {
-int data;
-Node * next;
-};
-
-void removeDuplicates(Node *lst) {
-unordered_map hashmap;
-struct Node *prev = lst;
-
-while(lst!=null){//loop through list
-if(hashmap.contains(lst.data)){//check if already present in hashmap
-                                prev  lst
-// 12->34->45->12->null
-prev->next=lst->next;
-lst=lst->next;
-}
-else{
-hashmap.add(lst.data);
-prev=lst;
-lst=lst->next;
-}
-}
-}
-
+include <unordered_map>
+using namespace std;
+
+struct Node {
+  int data;
+  Node * next;
+};
+
+void removeDuplicates(Node *lst) {
+  unordered_map hashmap;
+  struct Node *prev = lst;
+
+  while(lst!=null){//loop through list
+    if(hashmap.contains(lst.data)){//check if already present in hashmap
+                                    prev  lst
+      // 12->34->45->12->null
+      prev->next=lst->next;
+      lst=lst->next;
+    }
+    else{
+      hashmap.add(lst.data);
+      prev=lst;
+      lst=lst->next;
+    }
+  }
+}
