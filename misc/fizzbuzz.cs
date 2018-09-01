2c753d7b102ced5a5225b23658d092bd1124dd93
Shree Harsha Sridharamurthy <s.shreeharsha@gmail.com>
2018-09-01 17:48:44 -0500

    fizzbuzz

---
+++ misc/fizzbuzz.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string output = "";
+            for(int i=1;i<=100;i++){
+                if(i%3 == 0) {output = "fizz";}
+                if(i%5 == 0) {output += "buzz";}
+                if(output=="") {output += (i+" ");}
+                Console.WriteLine(output);
+                output="";
+            }
+        }
+    }
+}
